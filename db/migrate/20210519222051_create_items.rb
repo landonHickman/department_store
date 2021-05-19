9244fd542c5125e5af270823dc3537b69b15e6fc
landon hickman <landonhickman12@gmail.com>
2021-05-19 16:23:00 -0600

    added belongs_to model had to del and regenerate

--- db/migrate/20210519222051_create_items.rb
+++ db/migrate/20210519222051_create_items.rb
@@ -3,6 +3,7 @@ class CreateItems < ActiveRecord::Migration[6.1]
     create_table :items do |t|
       t.string :name
       t.float :price
+      t.belongs_to :department, null: false, foreign_key: true
 
       t.timestamps
     end
