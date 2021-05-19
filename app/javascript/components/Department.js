7d628d6e0615d613627be68048e624a9b69043d2
landon hickman <landonhickman12@gmail.com>
2021-05-19 16:01:42 -0600

    add margin to anchor tags

--- app/javascript/components/Department.js
+++ app/javascript/components/Department.js
@@ -8,7 +8,10 @@ const Department = (props) => {
       <h1>{`Department #${id}: ${name}`}</h1>
       <a href={`/departments/${id}/edit`}>Edit</a>
       <a href={`/departments/${id}`} data-method="delete">Delete</a>
-      <a href="/">Back to all Departments</a>
+      <div>
+        <h1></h1>
+        <a href="/">Back to all Departments</a>
+      </div>
     </div>
     </>
   )
